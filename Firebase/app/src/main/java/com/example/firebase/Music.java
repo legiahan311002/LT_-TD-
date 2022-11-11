cc9d806076ec6c76538c82ee0e8f4861ccca3f47
legiahan311002 <Legiahan311002@gmail.com>
2022-11-11 19:25:32 +0700

    FireBase

---
+++ Firebase/app/src/main/java/com/example/firebase/Music.java
@@ -0,0 +1,48 @@
+package com.example.firebase;
+
+public class Music {
+
+    String name,Singel, img, time;
+
+    Music() {
+
+    }
+
+    public Music(String name, String Singel, String img, String time) {
+        this.name = name;
+        this.Singel = Singel;
+        this.img = img;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSingel() {
+        return Singel;
+    }
+
+    public void setSingel(String Singel) {this.Singel = Singel;}
+
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
